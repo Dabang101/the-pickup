46c6367e65aff334cfb35b5a2de3a018d9d8b9cc
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-03 11:57:24 -0500

    Second Commit

--- main.py
+++ main.py
@@ -15,10 +15,40 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+        template = jinja_environment.get_template('templates/template.html')
+        self.response.out.write(template.render())
+
+
+class PickUpGame(ndb.Model):
+    sport = ndb.StringProperty()
+    time = ndb.DateTimeProperty()
+    location = ndb.KeyProperty(Location)
+    players = ndb.KeyProperty(Player, repeated=True)
+
+class Location(ndb.Model):
+    address = ndb.StringProperty()
+
+class Player(ndb.Model):
+    #link this to the Users API
+    nick_name = ndb.StringProperty()
+    age = ndb.IntegerProperty()
+    home_park = ndb.KeyProperty(Location)
+
+montrose_beach=Location(address="555 N Lake Shore Drive")
+wicker_park=Location(address="1600 N. Ashland")
+player1=Player(name="nicki", age="17", home_park=wicker_park.key)
+player2=Player(name="miles", age ="16", home_park=montrose_beach.key)
+bball1 = PickUpGame(sport = "basketball", time="5:00", location = montrose_beach.key, players= [player1.key, player2.key])
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
